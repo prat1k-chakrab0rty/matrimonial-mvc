32327d18252b27c525976f2834337e97fba29966
Pratik <pratikchakraborty2020@gmail.com>
2022-11-24 23:42:53 +0530

    Did layout in master page

---
+++ Matrimonial/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Matrimonial.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
